5d638b59a134f670ff83237f4fc305d739f8eaf3
haroldZS <haroldzs1024@gmail.com>
2024-08-26 23:28:25 -0400

    feat: Implement HashRouter redirection

--- src/components/HashRouter.js
+++ src/components/HashRouter.js
@@ -1,10 +1,17 @@
-import { createContext } from "react";
+import { createContext, useEffect } from "react";
 import { useLocation } from "../hooks/useLocation";
 
 const RouterContext = createContext();
 
 const HashRouter = ({ children }) => {
   const location = useLocation();
+
+  useEffect(() => {
+    if (!window.location.hash) {
+      window.history.replaceState(null, "", "#/");
+      window.dispatchEvent(new HashChangeEvent("hashchange"));
+    }
+  }, []);
 
   return (
     <RouterContext.Provider value={{ location }}>
